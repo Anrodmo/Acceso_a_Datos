616eb610d50ae01fca823888f6d8347e56758fee
Anrodmo <131297046+Anrodmo@users.noreply.github.com>
2023-11-03 22:39:22 +0100

    Se reorganiza y actualiza el main para practica 1.3
    Se modifica la clase Main para facilitar la identificación de qué elemento pertenece a qué práctica y se añade el código necesario para probar la practica  1.3

--- AD_UT1_Practica1.2/src/practica1_2/Practica1_2.java
+++ AD_UT1_Practica1.2/src/practica1_2/Practica1_2.java
@@ -16,7 +16,7 @@ public class Practica1_2 {
     public static void main(String[] args) {
         String ruta ="./src/practica1_2/libros.xml";       
         AccesoDOM acceso = new AccesoDOM();
-        
+        System.out.println("================== Práctica 1.1 ========================");
         if(ruta==null)ruta=""; // Evito la PointNullException al crear File
         File archivo = new File(ruta);
         
@@ -24,9 +24,16 @@ public class Practica1_2 {
             System.out.println("... DOM generado con éxito.\n");
         else
             System.out.println("...Error al creaar el DOM\n");
-             
+        System.out.println("\n================== Práctica 1.2 ========================");     
         acceso.recorreDOMyMuestra();
-        acceso.insertarLibroEnDOM("Vaca","Perro", "enero 20000 ");
+        System.out.println("\n================== Práctica 1.3 Añadir=================="); 
+        acceso.insertarLibroEnDOM("Vaca","Perro", "enero 2000 ");       
+        System.out.println("Mostrando DOM de nuevo para comprobar que se ha insertado con éxito:");
+        acceso.recorreDOMyMuestra();
+        
+        System.out.println("\n================== Práctica 1.3 Eliminar=================");
+        acceso.deleteNode("Vaca");
+        System.out.println("Mostrando DOM de nuevo para comprobar que se ha borrado con éxito:");
         acceso.recorreDOMyMuestra();
         
                 
