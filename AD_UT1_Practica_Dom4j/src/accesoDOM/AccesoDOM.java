31b6aea8c974e030246e769de34f931bd219d7d7
Anrodmo <131297046+Anrodmo@users.noreply.github.com>
2023-11-04 20:04:58 +0100

    Actualizado dom4j a practica 1.3
    Se añaden los métodos para añadir un libro y eliminarlo por titulo correspondientes a la práctica 1.3 pero con la librería DOM4J

--- AD_UT1_Practica_Dom4j/src/accesoDOM/AccesoDOM.java
+++ AD_UT1_Practica_Dom4j/src/accesoDOM/AccesoDOM.java
@@ -18,6 +18,7 @@ import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.io.DOMReader;
 import org.dom4j.io.SAXReader;
+import org.w3c.dom.NodeList;
 
 
 /**
@@ -44,21 +45,62 @@ public class AccesoDOM {
     }
     
     public void treeWalk(Element element) {
-    for (int i = 0, size = element.nodeCount(); i < size; i++) {
-        Node node = element.node(i);
-        
-        if (node instanceof Element) {
-            System.out.println("\t"+node.getName().trim()+" : "+node.getText().trim());
-            if(((Element) node).attributeCount()>0){
-                List<Attribute> attributes = ((Element) node).attributes();
-                for (Attribute attribute : attributes) {
-                    System.out.println("\t\t"+attribute.getName().trim() + " : " + attribute.getValue().trim());                   
+        for (int i = 0, size = element.nodeCount(); i < size; i++) {
+            Node node = element.node(i);
+
+            if (node instanceof Element) {
+                System.out.println("\t"+node.getName().trim()+" : "+node.getText().trim());
+                if(((Element) node).attributeCount()>0){
+                    List<Attribute> attributes = ((Element) node).attributes();
+                    for (Attribute attribute : attributes) {
+                        System.out.println("\t\t"+attribute.getName().trim() + " : " + attribute.getValue().trim());                   
+                    }
                 }
-            }
-            treeWalk((Element) node);           
-        }             
+                treeWalk((Element) node);
+                
+            }             
+        }
     }
-}
+    
+    public void insertarLibroEnDOM4J(String titulo, String autor, String fecha){
+       
+        // en DOM4j es al revés que en DOM, voy añadiendo directamente  al documento.
+        // extraigo el elemento raiz (Libros)
+        Element raiz = miDocumento.getRootElement();
+        // Creo Libro añadiendolo a la raiz.        
+        Element nLibro = raiz.addElement("Libro");
+        // y la añado a Libro su atributo.
+        nLibro.addAttribute("publicacion",fecha);
+
+        // Creo titulo y  lo añado a Libro, luego le pongo su valor
+        Element nTitulo = nLibro.addElement("Titulo");
+        nTitulo.setText(titulo);
+        // Creo autor y  lo añado a Libro, luego le pongo su valor
+        Element nAutor = nLibro.addElement("Autor");
+        nAutor.setText(autor);       
+    }
+    
+    public void borrarLibroPorTituloEnDOM4J(String titulo){
+        // Consigo el elemento raiz
+        Element raiz = miDocumento.getRootElement();
+        
+        
+        // Obtengo los elmentos Libro de la raiz y los recorro
+        for (Element elementoN1 : raiz.elements("Libro")) {
+            // Obtengo los elementos Titulo de cada elemento Libro
+            for (Element elementoN2 : elementoN1.elements("Titulo")){
+                //  si el valor del titulo coincide con el facilitado ...
+                if (elementoN2.getText().equals(titulo)){
+                    // Obtengo el padre del Libro en el que estoy y le quito el Libro.
+                    elementoN1.getParent().remove(elementoN1);
+                    // al quitar el Libro automaticammente  se eliminan todos los hijos y atributos
+
+                }             
+            }               
+        }       
+    }
+    
+    
     
     
 }
--- AD_UT1_Practica_Dom4j/src/practica_dom4j/Practica_Dom4j.java
+++ AD_UT1_Practica_Dom4j/src/practica_dom4j/Practica_Dom4j.java
@@ -25,11 +25,21 @@ public class Practica_Dom4j {
         if(ruta==null)ruta=""; // Evito la PointNullException al crear File
         File archivo = new File(ruta);
         
+        System.out.println("\n=========== Practica 1.1  y 1.2 - leo y muestro ===============\n");
         acceso.Parse(archivo);
         
         acceso.treeWalk();
         
+        System.out.println("\n=========== Practica 1.3 - añado y muestro ====================\n");
         
+        acceso.insertarLibroEnDOM4J("Libro añadido", "Angel", "2006");
+        acceso.treeWalk();
+        
+        
+        System.out.println("\n=========== Practica 1.3 - borro y muestro ====================\n");
+        
+        acceso.borrarLibroPorTituloEnDOM4J("Libro añadido");
+        acceso.treeWalk();
     }
     
 }
